b90c55a8c3f70746775ae1fefcf93548f140e53a
Mies van der lippe <miesvanderlippe@protonmail.com>
2024-12-01 16:57:18 +0100

    Day01, part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::PathBuf};
+use std::{collections::HashMap, fs, path::PathBuf};
 
 use clap::Parser;
 
@@ -13,8 +13,10 @@ fn main() {
 
     let file_contents = fs::read_to_string(args.input_file).expect("Unable to read file");
     let parsed_lists = parse_input(&file_contents).expect("Failed to parse file");
-    let result = calc_part_1(&parsed_lists);
-    println!("Total is: {}", result)
+    let part_1_result = calc_part_1(&parsed_lists);
+    let part_2_result = calc_part_2(&parsed_lists);
+    println!("Total distance is: {}", part_1_result);
+    println!("Total similarity is {}", part_2_result);
 }
 
 struct ParsedLists {
@@ -54,6 +56,41 @@ fn calc_part_1(parsed_lists: &ParsedLists) -> usize {
         .sum()
 }
 
+fn calc_part_2(parsed_lists: &ParsedLists) -> usize {
+    let mut right_count_lookup: HashMap<usize, usize> = HashMap::new();
+
+    let mut total_similarity = 0;
+    let mut peekable = parsed_lists.right.iter().peekable();
+
+    if let Some(first) = peekable.next() {
+        let mut cur = *first;
+        let mut count = 1;
+
+        loop {
+            let next = peekable.next();
+            if next.is_some_and(|&n| n == cur) {
+                count += 1;
+            } else {
+                right_count_lookup.insert(cur, count);
+
+                cur = *next.unwrap_or(&0);
+                count = 1;
+            }
+            if next == None {
+                break;
+            }
+        }
+    }
+
+    for num in &parsed_lists.left {
+        if let Some(right_count) = right_count_lookup.get(num) {
+            total_similarity += num * right_count
+        }
+    }
+
+    total_similarity
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -70,6 +107,22 @@ mod tests {
 
         let parsed_lists = parse_input(&test_input).unwrap();
         let result = calc_part_1(&parsed_lists);
+
+        assert_eq!(result, test_output)
+    }
+
+    #[test]
+    fn test_part_2() {
+        let test_input = r#"3   4
+4   3
+2   5
+1   3
+3   9
+3   3"#;
+        let test_output = 31;
+
+        let parsed_lists = parse_input(&test_input).unwrap();
+        let result = calc_part_2(&parsed_lists);
 
         assert_eq!(result, test_output)
     }
