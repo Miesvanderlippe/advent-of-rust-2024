00a3449738bc41240f45a9988dcfb85f11b47ef9
Mies van der lippe <miesvanderlippe@protonmail.com>
2024-12-04 19:07:24 +0100

    Day04, part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::Bytes;
 use std::path::PathBuf;
 
 use clap::Parser;
@@ -16,6 +17,67 @@ fn main() {
 
     let part_1 = solve_part_1(&file_contents, &"XMAS").expect("Could not solve the puzzle");
     println!("Part 1 answer {part_1}");
+
+    let part_2 = solve_part_2(&file_contents, &"MAS").expect("Could not solve the puzzle");
+    println!("Part 2 answer {part_2}");
+}
+
+fn byte_matcher(bytes: &[u8], start: usize, offsets: &[usize], search: &[u8]) -> bool {
+    // println!("Searching in {bytes:?} at start {start} for {search:?}");
+    for x in 0..search.len() {
+        if bytes[start + offsets[x]] != search[x] {
+            return false;
+        }
+    }
+    true
+}
+
+fn solve_part_2(puzzle: &str, search: &str) -> Result<usize, String> {
+    let puzzle_width = match puzzle.lines().next() {
+        Some(first_line) => first_line.bytes().len(),
+        None => {
+            return Err(String::from(
+                "Puzzle does not contain at least a first line",
+            ))
+        }
+    };
+    let puzzle_height = puzzle.bytes().filter(|b| *b == b'\n').count();
+    let puzzle_bytes: Vec<u8> = puzzle.bytes().filter(|b| b.is_ascii_alphabetic()).collect();
+
+    let search: Vec<u8> = search.bytes().collect();
+    let inverse_search: Vec<u8> = search.iter().rev().cloned().collect();
+
+    let search_len = search.len();
+
+    let mut matches = 0;
+
+    // Offsets to access first byte of row one, second byte of row two, third byte of row three.
+    let map_1: Vec<usize> = (0..search_len)
+        .map(|letter_index| (letter_index * puzzle_width) + letter_index)
+        .collect();
+    // Offsets for the inverse diagonal
+    let map_2: Vec<usize> = (0..search_len)
+        .map(|letter_index| (letter_index * puzzle_width) + (search_len - letter_index) - 1)
+        .collect();
+
+    for row in 0..=(puzzle_height - search_len) {
+        for col in 0..=(puzzle_width - search_len) {
+            let start = (row * puzzle_width) + col;
+
+            let match_map: [bool; 4] = [
+                byte_matcher(&puzzle_bytes, start, &map_1, &search),
+                byte_matcher(&puzzle_bytes, start, &map_1, &inverse_search),
+                byte_matcher(&puzzle_bytes, start, &map_2, &search),
+                byte_matcher(&puzzle_bytes, start, &map_2, &inverse_search),
+            ];
+
+            if match_map.iter().filter(|&is_match| *is_match).count() == 2 {
+                matches += 1;
+            }
+        }
+    }
+
+    Ok(matches)
 }
 
 fn solve_part_1(puzzle: &str, search: &str) -> Result<usize, String> {
@@ -118,6 +180,24 @@ MAMMMXMMMM
 MXMXAXMASX";
         let result = 18;
         let sum = solve_part_1(&puzzle, &word);
+        assert_eq!(Ok(result), sum);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let word = "MAS";
+        let puzzle = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        let result = 9;
+        let sum = solve_part_2(&puzzle, &word);
         assert_eq!(Ok(result), sum);
     }
 }
