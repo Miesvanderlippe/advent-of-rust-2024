ee4668e4ffb985a754dacdc5d04f59ca1e0129ad
Mies van der lippe <miesvanderlippe@protonmail.com>
2024-12-06 20:50:15 +0100

    Day05, part 2
    Sort is not perfect as described by my test case but it does work correctly for the problem.

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::{self, Display, Formatter};
 use std::fs;
 use std::path::PathBuf;
 
@@ -20,19 +21,35 @@ fn main() {
     let file_contents = fs::read_to_string(args.input_file).expect("Unable to read file");
     let (rules, manual) = parse_input(&file_contents);
 
-    let part_1_answer = solve_part_1(rules, manual);
+    let part_1_answer = solve_part_1(&rules, &manual);
     println!("Part 1: {part_1_answer}");
+
+    let part_2_answer = solve_part_2(&rules, &manual);
+    println!("Part 2: {part_2_answer}");
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 struct PageOrderingRule {
     left: usize,
     right: usize,
 }
 
+impl PageOrderingRule {
+    fn is_relevant_rule(&self, x: usize, y: usize) -> bool {
+        (self.left == x && self.right == y) || (self.left == y && self.right == x)
+    }
+}
+
+impl Display for PageOrderingRule {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.left, self.right)
+    }
+}
+
 trait PageOrderingRules {
     fn get_relevant_rules(&self, pages: &[usize]) -> Vec<&PageOrderingRule>;
     fn has_correct_order(&self, pages: &[usize]) -> bool;
+    fn sort_pages(&self, pages: &[usize]) -> Vec<usize>;
 }
 
 impl PageOrderingRules for Vec<PageOrderingRule> {
@@ -49,6 +66,30 @@ impl PageOrderingRules for Vec<PageOrderingRule> {
             pages.iter().position(|num| *num == rule.right)
                 > pages.iter().position(|num| *num == rule.left)
         })
+    }
+
+    fn sort_pages(&self, pages: &[usize]) -> Vec<usize> {
+        let relevant_rules = self.get_relevant_rules(&pages);
+
+        let mut result = Vec::from(pages);
+        result.sort_by(|a, b| {
+            match relevant_rules
+                .iter()
+                .filter(|&&r| r.is_relevant_rule(*a, *b))
+                .next()
+            {
+                Some(rule) => {
+                    if *a == rule.left {
+                        std::cmp::Ordering::Greater
+                    } else {
+                        std::cmp::Ordering::Less
+                    }
+                }
+                None => std::cmp::Ordering::Equal,
+            }
+        });
+
+        result
     }
 }
 
@@ -79,11 +120,24 @@ fn parse_input(input: &str) -> (Vec<PageOrderingRule>, Vec<Vec<usize>>) {
     (rules, manual)
 }
 
-fn solve_part_1(rules: Vec<PageOrderingRule>, manual: Vec<Vec<usize>>) -> usize {
+fn solve_part_1(rules: &Vec<PageOrderingRule>, manual: &Vec<Vec<usize>>) -> usize {
     let mut count = 0;
     for pagelist in manual {
         if rules.has_correct_order(&pagelist) {
             let middle_page = pagelist.get(pagelist.len().div_euclid(2)).unwrap();
+            count += middle_page;
+        }
+    }
+
+    count
+}
+
+fn solve_part_2(rules: &Vec<PageOrderingRule>, manual: &Vec<Vec<usize>>) -> usize {
+    let mut count = 0;
+    for pagelist in manual {
+        if !rules.has_correct_order(&pagelist) {
+            let sorted_pages = rules.sort_pages(&pagelist);
+            let middle_page = sorted_pages.get(sorted_pages.len().div_euclid(2)).unwrap();
             count += middle_page;
         }
     }
@@ -176,8 +230,44 @@ mod tests {
     #[test]
     fn test_part_1_first_col() {
         let (rules, manual) = parse_input(EXAMPLE_INPUT);
-        let solution = solve_part_1(rules, manual);
+        let solution = solve_part_1(&rules, &manual);
 
         assert_eq!(solution, 143);
+    }
+
+    #[test]
+    fn test_page_sort() {
+        let ruleset_1 = vec![
+            PageOrderingRule { left: 1, right: 2 },
+            PageOrderingRule { left: 3, right: 4 },
+            PageOrderingRule { left: 4, right: 1 },
+        ];
+
+        // Problematic operations
+        //                               1 | 5
+        // 1    5                        5 | 10
+        // 1    5     10
+        // 1    5     10                 5 | 9
+        // 1    5 9   10
+        // 1    5 9   10                 8 | 10
+        // 1    5 9 8 10
+        // 1    5 9 8 10                 8 | 9
+
+        // The two ends of the book are easy to find
+
+        // The only left with for which no right exists
+        // The only right with for which no left exists
+
+        // What rule to apply next?
+
+        assert_eq!(vec![3, 4, 1, 2], ruleset_1.sort_pages(&vec![1, 2, 3, 4]));
+    }
+
+    #[test]
+    fn test_part_2() {
+        let (rules, manual) = parse_input(EXAMPLE_INPUT);
+        let solution = solve_part_2(&rules, &manual);
+
+        assert_eq!(solution, 123);
     }
 }
