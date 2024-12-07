d764a03a802bc96f6e460cb6d730b6641922604a
Mies van der lippe <miesvanderlippe@protonmail.com>
2024-12-07 14:26:26 +0100

    Day06, part 1

---
+++ day06/src/main.rs
@@ -0,0 +1,277 @@
+use clap::Parser;
+use std::fmt::{self, Display, Formatter};
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, value_hint(clap::ValueHint::FilePath))]
+    input_file: PathBuf,
+    #[arg(short, long)]
+    display_solution: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let file_contents = fs::read_to_string(args.input_file).expect("Unable to read file");
+    let board = SituationMap::try_from(file_contents.as_ref()).expect("Expected a valid board");
+
+    println!("{}", board);
+
+    let part_1_answer = solve_part_1(board, args.display_solution);
+    println!("Part 1: {part_1_answer}");
+}
+
+fn solve_part_1(mut board: SituationMap, display_solution: bool) -> usize {
+    if display_solution {
+        println!("{board}")
+    }
+
+    let mut step_count: usize = 0;
+    let detailed_prints = 30 > (board.map_height + board.map_width);
+
+    loop {
+        match board.step() {
+            Some(step) => {
+                if display_solution {
+                    if detailed_prints || step_count % 8 == 0 {
+                        println!("Stepped to {}, {}", step.row, step.col);
+                        println!("{board}");
+                    }
+                    step_count += 1;
+                }
+            }
+            None => break,
+        }
+    }
+    board.seen_tiles()
+}
+
+struct Player {
+    orientation: Orientation,
+    coords: Coord,
+}
+
+#[derive(Clone, Copy)]
+struct Coord {
+    row: usize,
+    col: usize,
+}
+
+#[derive(PartialEq, Clone)]
+enum Orientation {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl Orientation {
+    fn rotate_right(&mut self) -> Self {
+        match self {
+            Orientation::Up => Orientation::Right,
+            Orientation::Right => Orientation::Down,
+            Orientation::Down => Orientation::Left,
+            Orientation::Left => Orientation::Up,
+        }
+    }
+}
+
+#[derive(PartialEq)]
+enum MapElements {
+    Free,
+    PrevouslySeen,
+    Obstructed,
+}
+
+struct SituationMap {
+    player: Player,
+    map: Vec<MapElements>,
+    map_width: usize,
+    map_height: usize,
+}
+
+impl SituationMap {
+    fn what_is_at(&self, coord: &Coord) -> Option<&MapElements> {
+        if coord.row > self.map_height {
+            None
+        } else if coord.col > self.map_width {
+            None
+        } else {
+            self.map.get((self.map_height * coord.row) + coord.col)
+        }
+    }
+
+    fn seen_tiles(&self) -> usize {
+        self.map
+            .iter()
+            .filter(|&t| t == &MapElements::PrevouslySeen)
+            .count()
+    }
+
+    fn what_is_in_front(&self) -> Option<(Coord, &MapElements)> {
+        let coords_in_front = match self.player.orientation {
+            Orientation::Up => Coord {
+                row: self.player.coords.row - 1,
+                col: self.player.coords.col,
+            },
+            Orientation::Right => Coord {
+                row: self.player.coords.row,
+                col: self.player.coords.col + 1,
+            },
+            Orientation::Down => Coord {
+                row: self.player.coords.row + 1,
+                col: self.player.coords.col,
+            },
+            Orientation::Left => Coord {
+                row: self.player.coords.row,
+                col: self.player.coords.col - 1,
+            },
+        };
+
+        let element_in_front = self.what_is_at(&coords_in_front)?;
+
+        Some((coords_in_front, element_in_front))
+    }
+
+    fn update_map(&mut self, at: &Coord, element: MapElements) {
+        if at.row > self.map_height || at.col > self.map_width {
+            panic!("Tried writing outside of map at {}, {}", at.col, at.row);
+        } else {
+            self.map[(self.map_height * at.row) + at.col] = element;
+        }
+    }
+
+    fn step(&mut self) -> Option<&Coord> {
+        loop {
+            match self.what_is_in_front() {
+                Some((coord, element)) => match element {
+                    MapElements::Free | MapElements::PrevouslySeen => {
+                        self.player.coords = coord;
+                        self.update_map(&self.player.coords.clone(), MapElements::PrevouslySeen);
+                        return Some(&self.player.coords);
+                    }
+                    MapElements::Obstructed => {
+                        self.player.orientation = self.player.orientation.rotate_right()
+                    }
+                },
+                // We went out of bounds, the desired end state.
+                None => return None,
+            };
+        }
+    }
+}
+
+impl Display for SituationMap {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for row in 0..self.map_height {
+            for col in 0..self.map_width {
+                match self.what_is_at(&Coord { row, col }) {
+                    Some(MapElements::Free) => write!(f, "\x1b[31;42m")?,
+                    Some(MapElements::PrevouslySeen) => write!(f, "\x1b[31;106m")?,
+                    Some(MapElements::Obstructed) => write!(f, "\x1b[31;40m")?,
+                    None => todo!(),
+                }
+
+                if self.player.coords.col == col && self.player.coords.row == row {
+                    match self.player.orientation {
+                        Orientation::Up => write!(f, "^")?,
+                        Orientation::Right => write!(f, ">")?,
+                        Orientation::Down => write!(f, "V")?,
+                        Orientation::Left => write!(f, "<")?,
+                    }
+                } else {
+                    write!(f, " ")?
+                }
+
+                write!(f, "\x1b[0m")?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
+
+impl TryFrom<&str> for SituationMap {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut player: Option<Player> = None;
+        let mut map = Vec::with_capacity(value.bytes().len());
+        let mut map_height = 0;
+        let map_width = if let Some(first_row) = value.lines().next() {
+            first_row.len()
+        } else {
+            return Err(String::from(
+                "Failed to get map width because the board does not contain at least one line.",
+            ));
+        };
+
+        for (row, line) in value.lines().enumerate() {
+            if line.len() != map_width {
+                return Err(format!("Line {line} is not of width {map_width}"));
+            }
+
+            map_height += 1;
+
+            for (col, c) in line.chars().enumerate() {
+                match c {
+                    '.' => map.push(MapElements::Free),
+                    '#' => map.push(MapElements::Obstructed),
+                    '^' => {
+                        map.push(MapElements::PrevouslySeen);
+
+                        match player {
+                            Some(p) => {
+                                return Err(format!(
+                                    "Duplicate player first at {}, {} then at {row}, {col}",
+                                    p.coords.row, p.coords.col
+                                ))
+                            }
+                            None => {
+                                player = Some(Player {
+                                    coords: Coord { row, col },
+                                    orientation: Orientation::Up,
+                                })
+                            }
+                        }
+                    }
+                    _ => return Err(format!("Map contains char {c} that we cannot parse")),
+                }
+            }
+        }
+
+        match player {
+            Some(player) => Ok(SituationMap {
+                player,
+                map,
+                map_height,
+                map_width,
+            }),
+            None => Err(String::from("Failed to detect player")),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
+    #[test]
+    fn test_part_1_example() {
+        let board = SituationMap::try_from(EXAMPLE_INPUT).unwrap();
+        assert_eq!(solve_part_1(board, true), 41);
+    }
+}
